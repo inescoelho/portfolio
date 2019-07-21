5a8ae9df031060d7233705452d0c6c4dc6b2d6a6
Ines Coelho <ines.opcoelho@gmail.com>
2019-07-21 19:34:59 +0100

    Updates

---
+++ precache-manifest.31f66a44d3afccdd254fa6c80756beee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016814f082d188b1bf63f8a53c91d8ff",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fd231d0f259c966a941b",
+    "url": "/portfolio/static/css/main.df7950f0.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fd231d0f259c966a941b",
+    "url": "/portfolio/static/js/main.4117345b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "69519cce5bd095d6148aabb876baaf14",
+    "url": "/portfolio/static/media/cover.69519cce.jpg"
+  }
+]);
